cf5cfb5bb73357d99775ad0e15a18efa77380245
Raghavtripathii <raghavtripathi4023@gmail.com>
2026-07-07 16:13:24 +0530

    Add signup endpoint to create new users

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,25 @@ app.get("/", (req, res) => {
   res.send("Vault Notes backend is alive!");
 });
 
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+
+  // basic check so we don't save empty accounts
+  if (!username || !password) {
+    return res.status(400).json({ error: "Username and password are required" });
+  }
+
+  const query = `INSERT INTO users (username, password) VALUES (?, ?)`;
+
+  db.run(query, [username, password], function (err) {
+    if (err) {
+      return res.status(400).json({ error: "That username is already taken" });
+    }
+
+    res.status(201).json({ message: "Account created!", userId: this.lastID });
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
