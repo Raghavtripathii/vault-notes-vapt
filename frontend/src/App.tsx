f465a4f86253a9c3fb6940465534bbd75258234f
Raghavtripathii <raghavtripathi4023@gmail.com>
2026-07-09 16:15:06 +0530

    Wire login into app and track auth token in state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import Signup from "./Signup";
+import Login from "./Login";
 
 function App() {
+  const [token, setToken] = useState("");
+
+  function handleLoginSuccess(newToken: string) {
+    setToken(newToken);
+  }
+
   return (
     <div>
       <h1>Vault Notes</h1>
-      <Signup />
+
+      {!token && (
+        <>
+          <Signup />
+          <Login onLoginSuccess={handleLoginSuccess} />
+        </>
+      )}
+
+      {token && (
+        <div>
+          <p>You are logged in!</p>
+        </div>
+      )}
     </div>
   );
 }
