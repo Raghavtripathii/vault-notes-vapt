678a199779b40c2c93100596873ee6fda973df36
Raghavtripathii <raghavtripathi4023@gmail.com>
2026-07-10 11:18:47 +0530

    VULN: expose backend JWT secret in frontend code (intentional)

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 // every request we make from the frontend goes to our backend,
+const JWT_SECRET_LEAKED_BY_MISTAKE = "this_is_a_super_secret_key_change_it_later";
+
 const api = axios.create({
   baseURL: "http://localhost:3000",
 });
