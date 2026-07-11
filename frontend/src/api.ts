533ce869f29bdfc4bb58097474bcb3e70f971288
Raghavtripathii <raghavtripathi4023@gmail.com>
2026-07-11 11:58:33 +0530

    Point frontend to deployed Render backend URL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const JWT_SECRET_LEAKED_BY_MISTAKE = "this_is_a_super_secret_key_change_it_later";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://vault-notes-backend.onrender.com",
 });
 
 export default api;
